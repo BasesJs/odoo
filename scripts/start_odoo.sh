726ac4ca9240d77d2c72636419b818077d82ea33
Basesjs <sameoldjake@gmail.com>
2024-07-04 07:16:43 -0400

    test

--- scripts/start_odoo.sh
+++ scripts/start_odoo.sh
@@ -4,7 +4,7 @@ exec odoo \
     --http-port="8069" \
     --init=all \
     --without-demo=True \
-    --proxy-mode=True \
+    --proxy-mode \
     --db_host="${ODOO_DATABASE_HOST}" \
     --db_port="${ODOO_DATABASE_PORT}" \
     --db_user="${ODOO_DATABASE_USER}" \
